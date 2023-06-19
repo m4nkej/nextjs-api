90adf133b10d873b55adcc23dd025f595f96eb3f
m4nkej <mankej91@yahoo.pl>
2023-06-19 09:19:40 +0200

    Changes to be committed: 	modified:   components/input/comments.js 	modified:   components/input/new-comment.js 	modified:   pages/api/comments/[eventId].js

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,23 +1,38 @@
 function hander(req, res) {
   const eventId = req.query.eventId;
   if (req.method === "POST") {
-    const { email, user, comment } = req.body;
+    const { email, name, text } = req.body;
+
     if (
-      !user ||
-      user.trim() === "" ||
-      !comment ||
-      comment.trim() === "" ||
+      !name ||
+      name.trim() === "" ||
+      !text ||
+      text.trim() === "" ||
       !email ||
       !email.includes("@")
     ) {
+      console.log(email, name, text);
       res.status(422).json({ message: "Invalid data." });
       return;
     }
-    const newComment = { id: new Date.toISOString(), email, user, comment };
-    console.log(email, user, comment);
-    res.status(201).json({ message: "Added", comment: newComment });
+    const newComment = { id: new Date().toISOString(), email, name, text };
+    console.log(newComment);
+    res.status(201).json({ message: "Added", text: newComment });
   }
   if (req.method === "GET") {
+    const dummyData = [
+      {
+        id: "c1",
+        name: "Max",
+        text: "test text",
+      },
+      {
+        id: "c2",
+        name: "Max2",
+        text: "test text",
+      },
+    ];
+    res.status(200).json(dummyData);
   }
 }
 
